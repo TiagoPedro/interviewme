cbc26fa6ae538fbdb3699097bcdefc81ab54e3dc
Tiago Pedro <tiago.miguel.pedro@gmail.com>
2017-05-06 01:36:23 +0100

    Removidos ficheiros não necessários e atualização do .gitignore. Versão funcional de emotion + speech.

---
+++ app/src/main/java/pt/iscte/interviewme/remoteservices/EmotionDetection.java
@@ -0,0 +1,8 @@
+package pt.iscte.interviewme.remoteservices;
+
+/**
+ * Created by tiago on 04/05/2017.
+ */
+
+public class EmotionDetection implements RemoteService {
+}
---
+++ app/src/main/java/pt/iscte/interviewme/remoteservices/helper/FaceDetection.java
@@ -0,0 +1,10 @@
+package pt.iscte.interviewme.remoteservices;
+
+/**
+ * Created by tiago on 04/05/2017.
+ */
+
+public class FaceDetection
+{
+    
+}
