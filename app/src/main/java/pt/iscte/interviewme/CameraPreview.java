a00647d43e178fc3f1aaf2c3ed157f52174eda19
Tiago Pedro <tiago.miguel.pedro@gmail.com>
2017-06-26 18:24:03 +0100

    Added: - Synchronizer for feeds - Splash screen requesting permissions - Speech Recognition class
    Removed:
    - BingSpeechToText class
    
    Changes:
    - SpeechRecognition class is similar to BingSpeechToText but the directs the Azure returned recognition to the MainActivity
    - Minor changes to CameraPreview and MainActivity

--- app/src/main/java/pt/iscte/interviewme/CameraPreview.java
+++ app/src/main/java/pt/iscte/interviewme/CameraPreview.java
@@ -2,28 +2,21 @@ package pt.iscte.interviewme;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
-import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.ImageView;
-
-import com.microsoft.projectoxford.emotion.contract.RecognizeResult;
 
 import pt.iscte.interviewme.remoteservices.EmotionDetection;
 
@@ -37,13 +30,10 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private PreviewCallback pc;
     private int cameraOrientaiton;
 
-    public CameraPreview(Context context, Camera camera, EmotionDetection emotionDetection,
-                         List<RecognizeResult> emotionResultsList, int cameraOrientation) {
+    public CameraPreview(Context context, Camera camera, EmotionDetection emotionDetection, int cameraOrientation) {
         super(context);
         mCamera = camera;
-        pc = new PreviewCallback(emotionDetection, emotionResultsList);
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
+        pc = new PreviewCallback(emotionDetection);
         mHolder = getHolder();
         mHolder.addCallback(this);
         this.cameraOrientaiton = cameraOrientation;
@@ -54,43 +44,44 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         return pc;
     }
 
-    public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, now tell the camera where to draw the preview.
-        try {
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        try
+        {
             mCamera.setPreviewDisplay(holder);
             mCamera.setDisplayOrientation(cameraOrientaiton);
             mCamera.setPreviewCallback(pc);
             mCamera.startPreview();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             Log.d("TAG", "Error setting camera preview: " + e.getMessage());
         }
     }
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
         // empty. Take care of releasing the Camera preview in your activity.
     }
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null){
-            // preview surface does not exist
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h)
+    {
+        if (mHolder.getSurface() == null)
+        {
             return;
         }
 
-        // stop preview before making changes
-        try {
+        try
+        {
             mCamera.stopPreview();
-        } catch (Exception e){
-            // ignore: tried to stop a non-existent preview
+        }
+        catch (Exception e)
+        {
+
         }
 
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-
-        // start preview with new settings
-        try {
+        try
+        {
             mCamera.setPreviewDisplay(mHolder);
             mCamera.setDisplayOrientation(cameraOrientaiton);
             mCamera.setPreviewCallback(pc);
@@ -109,13 +100,11 @@ class PreviewCallback implements Camera.PreviewCallback
     private Bitmap bmap;
     private long _baseTime = System.currentTimeMillis();
     private EmotionDetection emotionDetection;
-    private List<RecognizeResult> emotionResultsList;
     private Uri mUriPhotoTaken;
 
-    PreviewCallback(EmotionDetection emotionDetection, List<RecognizeResult> emotionResultsList)
+    PreviewCallback(EmotionDetection emotionDetection)
     {
         this.emotionDetection = emotionDetection;
-        this.emotionResultsList = emotionResultsList;
     }
 
     void setEmotionDetection(EmotionDetection emotionDetection)
@@ -128,7 +117,7 @@ class PreviewCallback implements Camera.PreviewCallback
     {
         if(emotionDetection != null)
         {
-            if(System.currentTimeMillis() - _baseTime >= 4*1000)
+            if(System.currentTimeMillis() - _baseTime >= 2*1000)
             {
                 Camera.Parameters parameters = camera.getParameters();
                 Camera.Size size = parameters.getPreviewSize();
@@ -144,34 +133,39 @@ class PreviewCallback implements Camera.PreviewCallback
                     bmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
 
                     Matrix matrix = new Matrix();
-                    matrix.postRotate(-90);
+                    if(MainActivity.CURRENT_CAMERA_ID == Camera.CameraInfo.CAMERA_FACING_FRONT)
+                        matrix.postRotate(-90);
+                    else
+                        matrix.postRotate(90);
                     Bitmap rotatedBmap = Bitmap.createBitmap(bmap, 0, 0, bmap.getWidth(), bmap.getHeight(), matrix, true);
 
                     File storageDir = Environment.getExternalStorageDirectory();
                     FileOutputStream fileStream = null;
                     File file = null;
-                    try {
+                    try
+                    {
                         file = File.createTempFile("IMG_", ".jpg", storageDir);
                         fileStream = new FileOutputStream(file);
-                    } catch (IOException e) {
-    //                    setInfo(e.getMessage());
                     }
-                    file.deleteOnExit();
+                    catch (IOException e)
+                    {
+//                        setInfo(e.getMessage());
+                    }
                     rotatedBmap.compress(Bitmap.CompressFormat.PNG, 50, fileStream);
                     mUriPhotoTaken = Uri.fromFile(file);
 
                     _baseTime = System.currentTimeMillis();
                 }
 
-
-                try {
-                    emotionDetection.recognizeImageEmotion(mUriPhotoTaken, emotionResultsList);
-                } catch (IOException e) {
+                try
+                {
+                    emotionDetection.recognizeImageEmotion(mUriPhotoTaken);
+                }
+                catch (IOException e)
+                {
                     e.printStackTrace();
                 }
             }
-            else
-                System.out.println("No emotion detector!");
         }
     }
 
--- app/src/main/java/pt/iscte/interviewme/MainActivity.java
+++ app/src/main/java/pt/iscte/interviewme/MainActivity.java
@@ -1,12 +1,8 @@
 package pt.iscte.interviewme;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.Context;
 import android.hardware.Camera;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,31 +12,49 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 
+import com.microsoft.bing.speech.SpeechClientStatus;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionResult;
 import com.microsoft.projectoxford.emotion.contract.Order;
 import com.microsoft.projectoxford.emotion.contract.RecognizeResult;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
-import pt.iscte.interviewme.remoteservices.BingSpeechToText;
 import pt.iscte.interviewme.remoteservices.EmotionDetection;
+import pt.iscte.interviewme.remoteservices.SpeechRecognition;
 
-public class MainActivity extends AppCompatActivity implements Observer
+import com.microsoft.cognitiveservices.speechrecognition.ISpeechRecognitionServerEvents;
+
+public class MainActivity extends AppCompatActivity implements Observer, ISpeechRecognitionServerEvents
 {
-    private HashMap<String,CheckBox> modalitiesMap;
+    // UI attributes
     private Button recordButton;
-    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    private Button switchCameraButton;
+    private EditText emotionFeedback;
+    private EditText speechFeedback;
+
+    // Logical attributes
+    private HashMap<String,CheckBox> modalitiesMap;
     private List<RecognizeResult> emotionResultsList;
     private EmotionDetection emotionDetection;
-    private EditText emotionFeedback;
+    private SpeechRecognition speechRecognition;
+    private Synchronizer sync;
+    private String previousString;
+    public static boolean IS_RECORDING = false;
+
+    // Camera attributes
     @SuppressWarnings("deprecation")
     private Camera mCamera;
     private CameraPreview mPreview;
+    private FrameLayout preview;
+    public static int CURRENT_CAMERA_ID = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -48,63 +62,164 @@ public class MainActivity extends AppCompatActivity implements Observer
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        requestPerms();
+        initialize();
+    }
 
-        emotionResultsList = null;
+    @Override
+    public void onBackPressed()
+    {
+        finishAndRemoveTask();
+    }
 
-        modalitiesMap = new HashMap<>();
-        modalitiesMap.put("speech",(CheckBox) findViewById(R.id.speechBox));
-        modalitiesMap.put("emotion", (CheckBox) findViewById(R.id.emotionBox));
+    private void setupEmotionDetector(Context context)
+    {
+        emotionDetection = new EmotionDetection(context);
+        emotionDetection.addObserver(MainActivity.this);
+        emotionFeedback.setText("");
+        emotionFeedback.setVisibility(View.VISIBLE);
+        mPreview.getPreviewCallback().setEmotionDetection(emotionDetection);
+    }
 
-        emotionFeedback = (EditText) findViewById(R.id.speechFeedback);
+    private void setupSpeechRecognition()
+    {
+        speechRecognition = new SpeechRecognition(this);
+    }
 
-        mCamera = getCameraInstance();
-        mPreview = new CameraPreview(this, mCamera, emotionDetection, emotionResultsList, setCameraDisplayOrientation(this, 1));
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+    private void startRecording(View view)
+    {
+        sync.openFile();
+
+        for(Map.Entry<String, CheckBox> entry : modalitiesMap.entrySet())
+        {
+            if(entry.getValue().isChecked())
+            {
+                switch(entry.getKey())
+                {
+                    case "speech":
+                        setupSpeechRecognition();
+                        speechRecognition.start();
+                        break;
+
+                    case "emotion":
+                        setupEmotionDetector(view.getContext());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void stopRecording()
+    {
+        if(emotionDetection != null)
+        {
+            mPreview.getPreviewCallback().setEmotionDetection(null);
+            emotionDetection = null;
+        }
+
+        sync.closeFile();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void switchCamera()
+    {
+        if (mCamera != null)
+        {
+            mCamera.stopPreview();
+            mCamera.setPreviewCallback(null);
+            mPreview.getHolder().removeCallback(mPreview);
+            mCamera.release();
+            mCamera = null;
+        }
+        preview.removeView(mPreview);
+
+        if(CURRENT_CAMERA_ID == Camera.CameraInfo.CAMERA_FACING_BACK)
+        {
+            CURRENT_CAMERA_ID = Camera.CameraInfo.CAMERA_FACING_FRONT;
+        }
+        else
+        {
+            CURRENT_CAMERA_ID = Camera.CameraInfo.CAMERA_FACING_BACK;
+        }
+        mCamera = Camera.open(CURRENT_CAMERA_ID);
+
+        setCameraDisplayOrientation(MainActivity.this, CURRENT_CAMERA_ID);
+
+        mPreview = new CameraPreview(this, mCamera, emotionDetection, setCameraDisplayOrientation(this, 1));
+        preview = (FrameLayout) findViewById(R.id.cameraPreview);
         preview.addView(mPreview);
+    }
 
+    private void setupButtons()
+    {
+        // Setting up record button
         recordButton = (Button) findViewById(R.id.recordButton);
         recordButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                for(Map.Entry<String, CheckBox> entry : modalitiesMap.entrySet())
+                IS_RECORDING = !IS_RECORDING;
+                if(IS_RECORDING)
                 {
-                    if(entry.getValue().isChecked())
-                    {
-                        switch(entry.getKey())
-                        {
-                            case "speech":
-                                startActivity(new Intent(view.getContext(), BingSpeechToText.class));
-                                break;
-
-                            case "emotion":
-                                emotionDetection = new EmotionDetection(view.getContext());
-                                emotionDetection.addObserver(MainActivity.this);
-                                emotionFeedback.setText("");
-                                emotionFeedback.setVisibility(View.VISIBLE);
-                                mPreview.getPreviewCallback().setEmotionDetection(emotionDetection);
-                                break;
-
-                            default:
-                                break;
-                        }
-                    }
+                    startRecording(view);
+                    recordButton.setBackgroundResource(R.mipmap.stop);
                 }
+                else
+                {
+                    stopRecording();
+                    recordButton.setBackgroundResource(R.mipmap.rec);
+                }
+            }
+        });
+
+        switchCameraButton = (Button) findViewById(R.id.cameraToggle);
+        switchCameraButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                switchCamera();
             }
         });
     }
 
+    private void initialize()
+    {
+        emotionResultsList = null;
+        previousString = "";
+        sync = new Synchronizer();
+
+        // Setting up hashmap with recognized modalities
+        modalitiesMap = new HashMap<>();
+        modalitiesMap.put("speech",(CheckBox) findViewById(R.id.speechBox));
+        modalitiesMap.put("emotion", (CheckBox) findViewById(R.id.emotionBox));
+
+        // Setting up TextViews for feedback
+        emotionFeedback = (EditText) findViewById(R.id.emotionFeedback);
+        speechFeedback = (EditText) findViewById(R.id.speechFeedback);
+
+        // Setting up Camera Preview and props
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(this, mCamera, emotionDetection, setCameraDisplayOrientation(this, 1));
+        preview = (FrameLayout) findViewById(R.id.cameraPreview);
+        preview.addView(mPreview);
+
+        setupButtons();
+    }
+
     @SuppressWarnings("deprecation")
-    public static int setCameraDisplayOrientation(Activity activity,
-                                                   int cameraId) {
+    public static int setCameraDisplayOrientation(Activity activity, int cameraId)
+    {
         Camera.CameraInfo info = new Camera.CameraInfo();
         Camera.getCameraInfo(cameraId, info);
         int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
         int degrees = 0;
 
-        switch (rotation) {
+        switch (rotation)
+        {
             case Surface.ROTATION_0: degrees = 0; break;
             case Surface.ROTATION_90: degrees = 90; break;
             case Surface.ROTATION_180: degrees = 180; break;
@@ -112,10 +227,13 @@ public class MainActivity extends AppCompatActivity implements Observer
         }
 
         int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT)
+        {
             result = (info.orientation + degrees) % 360;
             result = (360 - result) % 360;  // compensate the mirror
-        } else {  // back-facing
+        }
+        else
+        {  // back-facing
             result = (info.orientation - degrees + 360) % 360;
         }
 
@@ -127,79 +245,75 @@ public class MainActivity extends AppCompatActivity implements Observer
     {
         Camera c = null;
         try {
-            c = Camera.open(1); // attempt to get a Camera instance
+            c = Camera.open(CURRENT_CAMERA_ID);
         }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
+        catch (Exception e)
+        {
             Log.d("TAG", "Camera is not available (in use or does not exist)" + e.getMessage());
         }
-//        setCameraDisplayOrientation(this, 1, c);
-//        c.setDisplayOrientation(90);
+
         return c; // returns null if camera is unavailable
     }
 
-    private void requestPerms()
+    private String translateEmotion(String originalString)
     {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED)
+        String translatedString = "";
+        switch(originalString.toLowerCase())
         {
-            // No explanation needed, we can request the permission.
-
-            System.out.println("Self permission checked.");
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request
+            case "anger":
+                translatedString = getString(R.string.anger);
+                break;
+            case "contempt":
+                translatedString = getString(R.string.contempt);
+                break;
+            case "disgust":
+                translatedString = getString(R.string.disgust);
+                break;
+            case "fear":
+                translatedString = getString(R.string.fear);
+                break;
+            case "happiness":
+                translatedString = getString(R.string.happiness);
+                break;
+            case "neutral":
+                translatedString = getString(R.string.neutral);
+                break;
+            case "sadness":
+                translatedString = getString(R.string.sadness);
+                break;
+            case "surprise":
+                translatedString = getString(R.string.surprise);
+                break;
+            default:
+                break;
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults)
-    {
-        switch (requestCode)
-        {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
-            {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                }
-                else
-                {
-                    System.out.println("Record Audio permission denied!");
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
+        return translatedString;
     }
 
     public void postEmotionResults(List<RecognizeResult> result)
     {
-        int count = 0;
-
         for (RecognizeResult r : result)
         {
             List<Map.Entry<String, Double>> collection = r.scores.ToRankedList(Order.DESCENDING);
 
-            String detectedEmotion = collection.get(0).getKey();
-            Double emotionScore = collection.get(0).getValue();
+            String firstDetectedEmotion = collection.get(0).getKey();
+            String secondDetectedEmotion = collection.get(1).getKey();
 
-            emotionFeedback.append(String.format("\t %s: %f\n", detectedEmotion, emotionScore));
+            firstDetectedEmotion = translateEmotion(firstDetectedEmotion);
+            secondDetectedEmotion = translateEmotion(secondDetectedEmotion);
 
-            count++;
+            Double firstEmotionScore = collection.get(0).getValue() * 100;
+            Double secondEmotionScore = collection.get(1).getValue() * 100;
+            DecimalFormat df = new DecimalFormat("##.##");
+            df.setRoundingMode(RoundingMode.HALF_UP);
+
+            String output = String.format("%s: %s%%\t%s: %s%%\n",
+                    firstDetectedEmotion, df.format(firstEmotionScore),
+                    secondDetectedEmotion, df.format(secondEmotionScore));
+
+            emotionFeedback.append(output);
+
+            sync.writeToFile(output);
         }
     }
 
@@ -208,5 +322,64 @@ public class MainActivity extends AppCompatActivity implements Observer
     {
         List<RecognizeResult> result = (List<RecognizeResult>) arg;
         postEmotionResults(result);
+    }
+
+    /*********************************/
+    /******** Speech methods *********/
+    /*********************************/
+    @Override
+    public void onPartialResponseReceived(String response)
+    {
+        this.WriteLine(response);
+    }
+
+    @Override
+    public void onFinalResponseReceived(RecognitionResult response)
+    {
+        if(response.Results.length > 0)
+            this.WriteLine(response.Results[response.Results.length-1].DisplayText);
+
+        if(MainActivity.IS_RECORDING && response.Results.length > 0)
+        {
+            speechRecognition.stop();
+            setupSpeechRecognition();
+            speechRecognition.start();
+        }
+        else if(MainActivity.IS_RECORDING && response.Results.length == 0)
+        {
+            stopRecording();
+            recordButton.setBackgroundResource(R.mipmap.rec);
+        }
+        else
+        {
+            speechRecognition.stop();
+        }
+    }
+
+    @Override
+    public void onIntentReceived(String s)
+    {
+
+    }
+
+    @Override
+    public void onError(final int errorCode, final String response)
+    {
+        this.WriteLine("--- Error received by onError() ---");
+        this.WriteLine("Error code: " + SpeechClientStatus.fromInt(errorCode) + " " + errorCode);
+        this.WriteLine("Error text: " + response);
+        this.WriteLine("");
+    }
+
+    @Override
+    public void onAudioEvent(boolean recording)
+    {
+
+    }
+
+    private void WriteLine(String text)
+    {
+        speechFeedback.append(text + "\n");
+        sync.writeToFile(text + "\n");
     }
 }
